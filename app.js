8646fb538ee210cdc88da7f8fcdec30f06d77d22
Alex Star <astar@intertech.com>
2024-06-11 11:17:49 -0500

    user signup

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://alexstar1408:' + process.env.MONGO_ATLAS_PW + '@node-rest-shop.u7j7e5v.mongodb.net/?retryWrites=true&w=majority&appName=node-rest-shop')
 
 const productRoutes = require('./api/routes/products');
-const productOrders = require('./api/routes/orders');
+const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 app.use(morgan('dev'))
 app.use('/uploads', express.static('uploads'))
@@ -27,7 +28,8 @@ app.use((req, res, next) => {
 })
 
 app.use('/products', productRoutes)
-app.use('/orders', productOrders)
+app.use('/orders', orderRoutes)
+app.use('/users', userRoutes)
 
 app.use((req, res, next) => {
     const error = new Error('Not Found');
